d3cd3f4ca1c754d165371220b2eb358863d45ba1
Mark Peškov <mark.peshkov1@gmail.com>
2018-10-30 20:24:12 +0200

    feature/#31 Initialize MongoDB

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -5,12 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exceptions import DropItem
-
+import pymongo
+from scraper import settings
 
 class MagentoPipeline(object):
 
     def __init__(self):
-        self.ids_seen = set()
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
 
     # TODO 3rd iteration: Save data to MongoDB
     def process_item(self, item, spider):
